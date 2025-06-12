9e9da5c877d2ef98ec28876a9b5e2dd1401650f1
bruno <brunoteixeira@fastmail.com>
2025-06-12 14:08:54 -0300

    jogo

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,12 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export function getApostas() {
+  const divs = document.querySelectorAll("div.numeros-volante")
+  const apostas = Array.from(divs).map((div) =>
+    Array.from(div.querySelectorAll("span")).map((span) => span.textContent?.trim())
+  ).map((aposta) => aposta.join(" "));
+
+  console.log(apostas.join("\n"))
+}
